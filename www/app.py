74790fdec1bad57bb09146e6055310465cc9b01c
charles.yin <charles.yin@shopee.com>
2020-02-16 18:36:10 +0800

    update d2

--- www/app.py
+++ www/app.py
@@ -1,21 +1,20 @@
 import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from datetime import datetime
-from aiohttp import web
+from aiohttp import web, web_runner
 
+routes = web.RouteTableDef()
+
+@routes.get('/')
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+    return web.Response(body=b'<h1>Awesome Apps</h1>', content_type='text/html')
 
 
-@asyncio.coroutine
-def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+def init():
+    app = web.Application()
+    app.add_routes([web.get('/', index)])
     logging.info('server started at http://127.0.0.1:9000...')
-    return srv
+    web.run_app(app, host='127.0.0.1', port=9000)
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+init()
